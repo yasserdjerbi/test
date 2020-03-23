68564cdc8adc45e83d5354d4b94572c898da03c6
jobiols <jorge.obiols@gmail.com>
2020-03-23 17:01:11 -0300

    [WIP] modelo de dise~no de cheques

--- l10n_py_check_printing/models/check_layout.py
+++ l10n_py_check_printing/models/check_layout.py
@@ -12,21 +12,39 @@ class CheckLayout(models.Model):
         string='Nombre del diseño',
         required=True
     )
+    amount = fields.Boolean(
+        string="Total a pagar"
+    )
     amount_top = fields.Integer()
     amount_left = fields.Integer()
 
+    issue_date = fields.Boolean(
+        string="Fecha de emision"
+    )
     issue_date_top = fields.Integer()
     issue_date_left = fields.Integer()
 
+    payment_date = fields.Boolean(
+        string="Fecha de pago"
+    )
     payment_date_top = fields.Integer()
     payment_date_left = fields.Integer()
 
+    partner_name = fields.Boolean(
+        string="Destinatario"
+    )
     partner_name_top = fields.Integer()
     partner_name_left = fields.Integer()
 
+    amount_words = fields.Boolean(
+        string="Total en letras"
+    )
     amount_words_top = fields.Integer()
     amount_words_left = fields.Integer()
 
+    check_no = fields.Boolean(
+        string="Numero de cheque"
+    )
     check_no_top = fields.Integer()
     check_no_left = fields.Integer()
 
