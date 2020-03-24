71480a22602781829033cb599a4f80bf24e5a768
jobiols <jorge.obiols@gmail.com>
2020-03-24 15:56:22 -0300

    [WIP] ya esta imprimiendo cheques, aunque uno arriba del otro.

--- l10n_py_check_printing/models/account_payment.py
+++ l10n_py_check_printing/models/account_payment.py
@@ -11,21 +11,28 @@ _logger = logging.getLogger(__name__)
 class AccountPayment(models.Model):
     _inherit = 'account.payment'
 
-    def do_print_checks(self):
+    check_amount_in_words = fields.Char(
+        compute="_compute_amount_in_words"
+    )
 
+    def _compute_amount_in_words(self):
+        self.check_amount_in_words = 'quichicientos guaranies'
+
+    def do_print_checks(self):
         checkbook = self.mapped('checkbook_id')
         # si todos los cheques son de la misma chequera entonces buscamos
-        # reporte específico para esa chequera
+        # reporte específico para esa chequera o mandamos el standard.
         report_name = len(checkbook) == 1 and  \
             checkbook.report_template.report_name \
             or 'l10n_py_check_printing.print_check'
 
+        """
         if not checkbook.layout_id:
             raise UserError('Debe definir un Diseño de cheque para la '
                             'chequera '
                             'del banco %s' % checkbook.journal_id.name)
+        """
 
-        import wdb;wdb.set_trace()
 
         check_report = self.env['ir.actions.report'].search(
             [('report_name', '=', report_name)], limit=1).report_action(self)
