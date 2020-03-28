efb860c1f34d3d8a1e5f040f31b5d0eea1a0a13f
jobiols <jorge.obiols@gmail.com>
2020-03-28 01:10:16 -0300

    [FIX] Imprime los cheques

--- l10n_py_check_printing/wizard/print_check_wizard.py
+++ l10n_py_check_printing/wizard/print_check_wizard.py
@@ -7,7 +7,6 @@ class PrintCheckWizard(models.TransientModel):
     _name = "print.check.wizard"
 
     def print_checks(self):
-
         # cheques seleccionados de account.check
         ids = self.env.context['active_ids']
 
@@ -15,15 +14,7 @@ class PrintCheckWizard(models.TransientModel):
         ap = self.env['account.payment'].search([])
         ap = ap.filtered(lambda x: x.check_id.id in ids)
 
-        #import wdb;wdb.set_trace()
-        # imprimir los datos del cheque que estan en los pagos
-       #ap.print_checks()
-
-
-
-        #import wdb;wdb.set_trace()
-
         check_report = self.env['ir.actions.report'].search(
             [('report_name', '=', 'l10n_py_check_printing.print_check')])
 
-        return check_report.report_action(self)
+        return check_report.report_action(ap)
