8a3aeb47f73ba30280e7abea4eba26f44553cded
jobiols <jorge.obiols@gmail.com>
2020-03-28 00:34:39 -0300

    [WIP] esta imprimiendo al menos cheques en blanco.

--- l10n_py_check_printing/wizard/print_check_wizard.py
+++ l10n_py_check_printing/wizard/print_check_wizard.py
@@ -8,13 +8,22 @@ class PrintCheckWizard(models.TransientModel):
 
     def print_checks(self):
 
-        # cheques seleccionados
+        # cheques seleccionados de account.check
         ids = self.env.context['active_ids']
-        ap = self.env['account.payment'].search([])
 
         # dado el cheque buscar el pago
+        ap = self.env['account.payment'].search([])
         ap = ap.filtered(lambda x: x.check_id.id in ids)
 
         #import wdb;wdb.set_trace()
         # imprimir los datos del cheque que estan en los pagos
-        ap.print_checks()
+       #ap.print_checks()
+
+
+
+        #import wdb;wdb.set_trace()
+
+        check_report = self.env['ir.actions.report'].search(
+            [('report_name', '=', 'l10n_py_check_printing.print_check')])
+
+        return check_report.report_action(self)
