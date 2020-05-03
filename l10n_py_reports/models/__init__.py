5e7ba87385752e233bc3e974dcbdf86db49a1ea7
jobiols <jorge.obiols@gmail.com>
2020-05-03 18:23:49 -0300

    [WIP] agregads todos los modelos.

--- l10n_py_reports/models/__init__.py
+++ l10n_py_reports/models/__init__.py
@@ -1,6 +1,5 @@
 # For copyright and license notices, see __manifest__.py file in module root
 
-# TODO quitamos todos los modelos
-#from . import account_payment_group
-#from . import account_move_line
-#from . import l10n_latam_document_type
+from . import account_payment_group
+from . import account_move_line
+from . import l10n_latam_document_type
